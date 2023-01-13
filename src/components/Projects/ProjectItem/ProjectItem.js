3c1f48156f0457712ec01ceaa03d658982e33577
Dmitry Shishkov <99988422+DimDimShishkov@users.noreply.github.com>
2023-01-13 12:51:14 +0400

    feat: gif and mp4 of projects

--- src/components/Projects/ProjectItem/ProjectItem.js
+++ src/components/Projects/ProjectItem/ProjectItem.js
@@ -4,12 +4,16 @@ import "./ProjectItem.css";
 function ProjectItem({ project, setCurrentProject }) {
   return (
     <div className="projects__item" onClick={() => setCurrentProject(project)}>
-      <img
+      {/* <img 
         src={project.img}
         alt={project.alt}
         loading="lazy"
         className="projects__image"
-      />
+      /> */}
+      <video width="100%" height="193" autoplay muted>
+        <source src={project.img} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
       <div className="projects__textcontainer">
         <div className="projects__texts">
           <h3 className="projects__title">{project.title}</h3>
